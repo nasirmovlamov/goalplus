53c5cfadde15cc7242f8532e64b8075d9218c4b2
Nasir Movlamov <movlamovnasir2000@gmail.com>
2023-05-08 20:14:08 +0400

    feat: function name

--- src/pages/401.tsx
+++ src/pages/401.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-export default function () {
+export default function Page401() {
   useEffect(() => {
     window.location.href = "http://www.goalplus.az";
   }, []);
